8b5f006bf0606ead3c032289f2e411b36ec37bbe
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2024-11-05 11:19:01 +0100

    gjorde några uppgifter idk

---
+++ Näsan/Program.cs
@@ -0,0 +1,34 @@
+﻿if (6 > 3)
+{
+    Console.WriteLine("Hello, world!");
+}
+// -------------------------------------------------
+string namn = Console.ReadLine();
+string password = Console.ReadLine();
+
+if (namn == "noname" && password == "nopass")
+{
+    Console.WriteLine("Welcome!");
+}
+
+else
+{
+    Console.WriteLine("Wrong username or password");
+}
+// -------------------------------------------------
+int x = 0;
+
+while (x  <= 32)
+{
+    x++;
+    Console.WriteLine("Hello, world!");
+}
+// -------------------------------------------------
+password = Console.ReadLine();
+
+while (password != "password")
+{
+    Console.WriteLine("Wrong!");
+    password = Console.ReadLine();
+}
+// -------------------------------------------------
